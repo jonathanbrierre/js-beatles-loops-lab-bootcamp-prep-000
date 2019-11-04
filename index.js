b291334a1968ebf9e04c4f2875767fc5f560cdbd
jonathan brierre <jonathanbrierre@yahoo.com>
2019-11-04 01:51:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 function theBeatlesPlay(musicians,instruments){
   var newArray = [];
   for(var i=0;i<musicians.length; i++){
-    newArray.push(console.log(`${musicians[i]} plays ${instruments[i]}`));
+    newArray.push(`${musicians[i]} plays ${instruments[i]}`);
   }
   return newArray
 }
